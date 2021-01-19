c5e6ad9c71478ec12690aecfaabbe7a05cb39fa3
Savelyev Ivan <savelyev.exceedteam@gmail>
2021-01-19 17:08:14 +0300

    создание Store

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, {subscribe} from "./redux/state";
-import {addPost, updateNewPostText} from './redux/state'
+import store from "./redux/state";
 
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
     ReactDOM.render(
-        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />,
+        <App state={store.getState()} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />,
         document.getElementById('root')
     );
 }
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,68 +1,56 @@
-// const dialogsData = [
-//     {name: 'Dimich', id: 1},
-//     {name: 'Sveta', id: 2},
-//     {name: 'Goga', id: 3}
-// ]
-//
-// const messageData = [
-//     {message: 'BlaBla', id: 1},
-//     {message: 'BlaBla1', id: 2},
-//     {message: 'BlaBla2', id: 3},
-// ]
-//
-// const postData = [
-//     {message: 'Hi, how are you?', likesCount: 10},
-//     {message: 'It\'s my first post', likesCount: 15},
-//     {message: 'It\'s my first post', likesCount: 15},
-// ]
+const store = {
+    _state: {
+        profilePage: {
+            messages: [
+                {message: 'Hi, how are you?', likesCount: 10},
+                {message: 'It\'s my first post', likesCount: 15},
+                {message: 'It\'s my first post', likesCount: 15},
+            ],
+            newPostText: 'it-kamasutra',
+        },
 
-let rerenderEntireTree = () => {
-    console.log('State')
-}
-
-const state = {
-    profilePage: {
-        messages: [
-            {message: 'Hi, how are you?', likesCount: 10},
-            {message: 'It\'s my first post', likesCount: 15},
-            {message: 'It\'s my first post', likesCount: 15},
-        ],
-        newPostText: 'it-kamasutra',
+        dialogsPage: {
+            posts: [
+                {name: 'Dimich', id: 1},
+                {name: 'Sveta', id: 2},
+                {name: 'Goga', id: 3}
+            ],
+            messages:[
+                {message: 'BlaBla', id: 1},
+                {message: 'BlaBla1', id: 2},
+                {message: 'BlaBla2', id: 3},
+            ],
+        },
     },
 
-    dialogsPage: {
-        posts: [
-            {name: 'Dimich', id: 1},
-            {name: 'Sveta', id: 2},
-            {name: 'Goga', id: 3}
-        ],
-        messages:[
-            {message: 'BlaBla', id: 1},
-            {message: 'BlaBla1', id: 2},
-            {message: 'BlaBla2', id: 3},
-        ],
+    getState() {
+        return this._state
+    },
+
+    _callSubscriber() {
+        console.log('State')
+    },
+
+    addPost(postMessage) {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 7,
+        }
+        // console.log(newPost)
+        this._state.profilePage.messages.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state)
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText
+        this._callSubscriber(this._state)
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer
     },
 }
 
-export let addPost = (postMessage) => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 7,
-    }
-    // console.log(newPost)
-    state.profilePage.messages.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText
-    rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer
-}
-
-export default state
+export default store
